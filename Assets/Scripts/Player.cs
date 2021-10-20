057d23ecf478e3a50ff56017f9a82790254b0037
03-LiTingLim <91869598+03-LiTingLim@users.noreply.github.com>
2021-10-20 13:01:05 +0800

    Update PlayerScript

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
     public float speed;
-
+    private Rigidbody rb;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        transform.position = new Vector3(transform.position.x,
+                                       Mathf.Clamp(transform.position.y, -3f, 3f),
+                                        transform.position.z);
         if (Input.GetKeyDown(KeyCode.Space))
             thisAnimation.Play();
-        transform.position += new Vector3(0, 2, 0) * Time.deltaTime;
-    
     }
 }
